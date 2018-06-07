4e28bf2d736050d4677a7f8b7815efb871154c2c
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-07 19:42:35 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+num = [1,2,3,4]
+
+def my_each(num)
+  counter = 0
+  while counter < num.length
+    yield(num[0])
+    counter += 1
+    yield(num[1])
+    counter += 1
+    yield(num[2])
+    counter += 1
+    yield(num[3])
+  end
+end
+
+my_each(num) do |i|
+  puts i
 end
